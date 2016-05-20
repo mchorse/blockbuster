5a47fcb73a95ca846539315878e425dc10e641c5
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-05-20 18:46:37 +0100

    Camera item added

--- main/java/com/noname/blockbuster/BlockbusterMod.java
+++ main/java/com/noname/blockbuster/BlockbusterMod.java
@@ -1,14 +1,18 @@
 package com.noname.blockbuster;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Blockbuster's main entry
@@ -24,9 +28,26 @@ public class BlockbusterMod
     public static final String MODNAME = "Blockbuster";
     public static final String VERSION = "1.0";
     
-    @EventHandler 
+    public static Item camera;
+    
+    public static final CreativeTabs busterTab = new CreativeTabs("blockbusterTab") 
+	{
+		@Override
+		public Item getTabIconItem() 
+		{
+			return BlockbusterMod.camera;
+		} 
+	};
+    
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
     	// Register camera, actors and props eggs
+    	camera = new CameraItem().setUnlocalizedName("cameraItem")
+    							 .setRegistryName("cameraItem")
+    							 .setCreativeTab(busterTab);
+    	
+    	GameRegistry.register(camera);
+    	ModelLoader.setCustomModelResourceLocation(camera, 0, new ModelResourceLocation("blockbuster:cameraItem", "inventory"));
     }
 }
---
+++ main/java/com/noname/blockbuster/CameraItem.java
@@ -0,0 +1,33 @@
+package com.noname.blockbuster;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * Camera item
+ * 
+ * Spawns a rideable camera entity (for controlling the camera)
+ */
+public class CameraItem extends Item 
+{
+	public CameraItem()
+	{
+		setMaxStackSize(1);
+	}
+	
+	/**
+	 * Spawns a camera 
+	 */
+	@Override
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) 
+	{
+		return super.onItemUse(stack, playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+	}
+}
