2836742ab3e0bbedcd28fb693a51c6fd6d22bd4e
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-06-03 09:55:32 +0100

    Small refactor

--- main/java/noname/blockbuster/recording/MocapEventHandler.java
+++ main/java/noname/blockbuster/recording/MocapEventHandler.java
@@ -31,8 +31,6 @@ public class MocapEventHandler
                 ma.target = event.getEntityBeingMounted().getUniqueID();
                 ma.armorSlot = event.isMounting() ? 1 : 0;
                 aList.add(ma);
-
-                System.out.println(ma.target + " " + ma.armorSlot);
             }
         }
     }
@@ -40,71 +38,71 @@ public class MocapEventHandler
     @SubscribeEvent
     public void onPlayerLoggedOutEvent(PlayerEvent.PlayerLoggedOutEvent event)
     {
-        Side side = FMLCommonHandler.instance().getEffectiveSide();
-
-        if (side == Side.SERVER)
+        if (FMLCommonHandler.instance().getEffectiveSide() != Side.SERVER)
         {
-            List<Action> aList = Mocap.getActionListForPlayer(event.player);
+            return;
+        }
 
-            if (aList != null)
-            {
-                Action ma = new Action(Action.LOGOUT);
-                aList.add(ma);
-            }
+        List<Action> aList = Mocap.getActionListForPlayer(event.player);
+
+        if (aList != null)
+        {
+            Action ma = new Action(Action.LOGOUT);
+            aList.add(ma);
         }
     }
 
     @SubscribeEvent
     public void onArrowLooseEvent(ArrowLooseEvent ev) throws IOException
     {
-        Side side = FMLCommonHandler.instance().getEffectiveSide();
-
-        if (side == Side.SERVER)
+        if (FMLCommonHandler.instance().getEffectiveSide() != Side.SERVER)
         {
-            List<Action> aList = Mocap.getActionListForPlayer(ev.getEntityPlayer());
+            return;
+        }
 
-            if (aList != null)
-            {
-                Action ma = new Action(Action.SHOOTARROW);
-                ma.arrowCharge = ev.getCharge();
-                aList.add(ma);
-            }
+        List<Action> aList = Mocap.getActionListForPlayer(ev.getEntityPlayer());
+
+        if (aList != null)
+        {
+            Action ma = new Action(Action.SHOOTARROW);
+            ma.arrowCharge = ev.getCharge();
+            aList.add(ma);
         }
     }
 
     @SubscribeEvent
     public void onItemTossEvent(ItemTossEvent ev) throws IOException
     {
-        Side side = FMLCommonHandler.instance().getEffectiveSide();
-
-        if (side == Side.SERVER)
+        if (FMLCommonHandler.instance().getEffectiveSide() != Side.SERVER)
         {
-            List<Action> aList = Mocap.getActionListForPlayer(ev.getPlayer());
+            return;
+        }
 
-            if (aList != null)
-            {
-                Action ma = new Action(Action.DROP);
-                ev.getEntityItem().getEntityItem().writeToNBT(ma.itemData);
-                aList.add(ma);
-            }
+        List<Action> aList = Mocap.getActionListForPlayer(ev.getPlayer());
+
+        if (aList != null)
+        {
+            Action ma = new Action(Action.DROP);
+            ev.getEntityItem().getEntityItem().writeToNBT(ma.itemData);
+            aList.add(ma);
         }
     }
 
     @SubscribeEvent
     public void onServerChatEvent(ServerChatEvent ev)
     {
-        Side side = FMLCommonHandler.instance().getEffectiveSide();
-
-        if (side == Side.SERVER)
+        if (FMLCommonHandler.instance().getEffectiveSide() != Side.SERVER)
         {
-            List<Action> aList = Mocap.getActionListForPlayer(ev.getPlayer());
+            return;
+        }
 
-            if (aList != null)
-            {
-                Action ma = new Action(Action.CHAT);
-                ma.message = ev.getMessage();
-                aList.add(ma);
-            }
+        List<Action> aList = Mocap.getActionListForPlayer(ev.getPlayer());
+
+        if (aList != null)
+        {
+            Action ma = new Action(Action.CHAT);
+            ma.message = ev.getMessage();
+            aList.add(ma);
         }
     }
 }
