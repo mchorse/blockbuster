cf2e3df51e4be3403a4a057420592f532660de3f
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-07-13 20:30:31 +0100

    Change version across files

--- src/main/java/noname/blockbuster/Blockbuster.java
+++ src/main/java/noname/blockbuster/Blockbuster.java
@@ -38,7 +38,7 @@ public class Blockbuster
     /* Mod info */
     public static final String MODID = "blockbuster";
     public static final String MODNAME = "Blockbuster";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2-dev";
 
     /* Items */
     public static Item cameraItem;
