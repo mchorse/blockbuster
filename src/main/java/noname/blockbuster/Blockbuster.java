4b9f8a11cb3915615bfdbdfb66055f742192b8ee
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-07-26 07:59:21 +0100

    Nevermind, first goes the pre-release

--- src/main/java/noname/blockbuster/Blockbuster.java
+++ src/main/java/noname/blockbuster/Blockbuster.java
@@ -37,7 +37,7 @@ public class Blockbuster
     /* Mod info */
     public static final String MODID = "blockbuster";
     public static final String MODNAME = "Blockbuster";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2-pre";
 
     /* Items */
     public static Item playbackItem;
