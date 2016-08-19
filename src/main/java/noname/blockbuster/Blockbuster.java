78ef2815686494eda428c4ea028da07f32f3c48f
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-08-19 09:36:07 +0100

    Change version to 1.2.1

--- src/main/java/noname/blockbuster/Blockbuster.java
+++ src/main/java/noname/blockbuster/Blockbuster.java
@@ -37,7 +37,7 @@ public class Blockbuster
     /* Mod info */
     public static final String MODID = "blockbuster";
     public static final String MODNAME = "Blockbuster";
-    public static final String VERSION = "1.2.1-pre";
+    public static final String VERSION = "1.2.1";
 
     /* Items */
     public static Item playbackItem;
