bc3aa7c60bbaeb1fde647609244d447510d8e9a0
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-09-27 21:30:10 +0100

    Update version number to 1.3

--- src/main/java/mchorse/blockbuster/common/Blockbuster.java
+++ src/main/java/mchorse/blockbuster/common/Blockbuster.java
@@ -39,7 +39,7 @@ public class Blockbuster
     /* Mod info */
     public static final String MODID = "blockbuster";
     public static final String MODNAME = "Blockbuster";
-    public static final String VERSION = "1.3-pre";
+    public static final String VERSION = "1.3";
 
     /* Items */
     public static Item playbackItem;
