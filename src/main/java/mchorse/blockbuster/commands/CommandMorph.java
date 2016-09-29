0398cf0cf44786455e001a75970ccf58680a7933
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-09-29 09:08:49 +0100

    Add tab completition for morph command

--- src/main/java/mchorse/blockbuster/commands/CommandMorph.java
+++ src/main/java/mchorse/blockbuster/commands/CommandMorph.java
@@ -1,11 +1,16 @@
 package mchorse.blockbuster.commands;
 
+import java.util.Collections;
+import java.util.List;
+
+import mchorse.blockbuster.common.ClientProxy;
 import mchorse.blockbuster.network.Dispatcher;
 import mchorse.blockbuster.network.common.PacketMorph;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 
 /**
@@ -47,5 +52,20 @@ public class CommandMorph extends CommandBase
 
             sender.addChatMessage(new TextComponentTranslation("blockbuster.morph", args[0]));
         }
+    }
+
+    @Override
+    public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos pos)
+    {
+        if (args.length == 1)
+        {
+            return getListOfStringsMatchingLastWord(args, ClientProxy.actorPack.pack.getModels());
+        }
+        else if (args.length == 2)
+        {
+            return getListOfStringsMatchingLastWord(args, ClientProxy.actorPack.pack.getSkins(args[0]));
+        }
+
+        return Collections.<String> emptyList();
     }
 }
