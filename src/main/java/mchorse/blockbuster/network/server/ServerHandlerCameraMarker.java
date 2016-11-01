18be961c9dd6125f642428eb5e2fdbbb5f3cdd39
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-11-01 09:25:40 +0000

    Update camera marker with multiplier

--- src/main/java/mchorse/blockbuster/network/server/ServerHandlerCameraMarker.java
+++ src/main/java/mchorse/blockbuster/network/server/ServerHandlerCameraMarker.java
@@ -16,9 +16,11 @@ public class ServerHandlerCameraMarker extends ServerMessageHandler<PacketCamera
 
         if (record == null) return;
 
-        String difference = Integer.toString(record.tick - record.previousTick);
-        String tick = Integer.toString(record.tick);
-        String string = String.format("§6§l|§r Record §7%s§r, §7%s§6t§r §8(§rdifference in §7%s§6t§r§8)§r.", record.record.filename, tick, difference);
+        int delay = record.record.delay;
+
+        String difference = Integer.toString((record.tick - record.previousTick) * delay);
+        String tick = Integer.toString(record.tick * delay);
+        String string = String.format("§6§l|§r Record §7%s§r §8(§6x%s§8)§r, §7%s§6t§r §8(§rdifference in §7%s§6t§r§8)§r.", record.record.filename, Integer.toString(delay), tick, difference);
 
         CommonProxy.manager.getActions(player).add(new ChatAction(string));
         player.addChatMessage(new TextComponentString(string));
