2262283822d696e54fe504609e719294f3202d42
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-11-03 14:54:32 +0000

    The 'difference is' string

--- src/main/java/mchorse/blockbuster/network/server/ServerHandlerCameraMarker.java
+++ src/main/java/mchorse/blockbuster/network/server/ServerHandlerCameraMarker.java
@@ -4,6 +4,7 @@ import mchorse.blockbuster.common.CommonProxy;
 import mchorse.blockbuster.network.common.PacketCameraMarker;
 import mchorse.blockbuster.recording.RecordRecorder;
 import mchorse.blockbuster.recording.actions.ChatAction;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.text.TextComponentString;
 
@@ -20,7 +21,7 @@ public class ServerHandlerCameraMarker extends ServerMessageHandler<PacketCamera
 
         String difference = Integer.toString((record.tick - record.previousTick) * delay);
         String tick = Integer.toString(record.tick * delay);
-        String string = String.format("§6§l|§r Record §7%s§r §8(§6x%s§8)§r, §7%s§6t§r §8(§rdifference in §7%s§6t§r§8)§r.", record.record.filename, Integer.toString(delay), tick, difference);
+        String string = I18n.format("blockbuster.marker", record.record.filename, Integer.toString(delay), tick, difference);
 
         CommonProxy.manager.getActions(player).add(new ChatAction(string));
         player.addChatMessage(new TextComponentString(string));
