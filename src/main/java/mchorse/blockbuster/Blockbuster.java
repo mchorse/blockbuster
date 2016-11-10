5527a7aa99d02f6b8a95c10d6902fb4931bdf371
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-11-10 08:05:10 +0000

    Change version to 1.4

--- src/main/java/mchorse/blockbuster/Blockbuster.java
+++ src/main/java/mchorse/blockbuster/Blockbuster.java
@@ -53,7 +53,7 @@ public class Blockbuster
     /* Mod info */
     public static final String MODID = "blockbuster";
     public static final String MODNAME = "Blockbuster";
-    public static final String VERSION = "1.3.1-pre";
+    public static final String VERSION = "1.4";
     public static final String GUI_FACTORY = "mchorse.blockbuster.config.gui.GuiFactory";
 
     /* Proxies */
