d2236cb9677b870061a49465cc845404abbf4889
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-01-18 12:23:52 +0000

    Bump version

--- src/main/java/mchorse/blockbuster/Blockbuster.java
+++ src/main/java/mchorse/blockbuster/Blockbuster.java
@@ -51,7 +51,7 @@ public class Blockbuster
     /* Mod info */
     public static final String MODID = "blockbuster";
     public static final String MODNAME = "Blockbuster";
-    public static final String VERSION = "1.4.6";
+    public static final String VERSION = "1.4.7";
     public static final String GUI_FACTORY = "mchorse.blockbuster.config.gui.GuiFactory";
 
     /* Proxies */
