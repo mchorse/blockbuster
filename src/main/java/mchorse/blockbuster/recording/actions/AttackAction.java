541becdda4f988e66eed3bbede15d8a50ae65b32
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-02-22 08:30:24 +0000

    Fix angle of attack

--- src/main/java/mchorse/blockbuster/recording/actions/AttackAction.java
+++ src/main/java/mchorse/blockbuster/recording/actions/AttackAction.java
@@ -9,7 +9,7 @@ import net.minecraft.util.DamageSource;
 /**
  * Attack action
  *
- * This action is responsible for attacking an entity in in front of the
+ * This action is responsible for attacking an entity in in front of the actor.
  */
 public class AttackAction extends Action
 {
@@ -28,14 +28,17 @@ public class AttackAction extends Action
         Frame frame = actor.playback.record.frames.get(actor.playback.tick);
         float yaw = actor.rotationYaw;
         float pitch = actor.rotationPitch;
+        float yawHead = actor.rotationYawHead;
 
         actor.rotationYaw = frame.yaw;
         actor.rotationPitch = frame.pitch;
+        actor.rotationYawHead = frame.yawHead;
 
         Entity target = EntityUtils.getTargetEntity(actor, 5.0);
 
         actor.rotationYaw = yaw;
         actor.rotationPitch = pitch;
+        actor.rotationYawHead = yawHead;
 
         if (target != null)
         {
