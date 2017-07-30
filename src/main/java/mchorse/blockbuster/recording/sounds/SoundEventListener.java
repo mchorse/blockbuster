0b212a82d457c05005a9b0967322097f0b619ede
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-07-30 11:42:26 +0100

    Sound recording

--- src/main/java/mchorse/blockbuster/recording/sounds/SoundEventListener.java
+++ src/main/java/mchorse/blockbuster/recording/sounds/SoundEventListener.java
@@ -1,7 +1,9 @@
 package mchorse.blockbuster.recording.sounds;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.SoundCategory;
@@ -19,16 +21,27 @@ public class SoundEventListener implements IWorldEventListener
 {
     public static SoundEventListener INSTANCE = null;
 
-    public SoundSession session;
-    public Minecraft mc;
+    public Map<EntityPlayer, SoundSession> sessions = new HashMap<EntityPlayer, SoundSession>();
     public World world;
-
-    public int frame;
 
     public SoundEventListener(World world)
     {
-        this.mc = Minecraft.getMinecraft();
         this.world = world;
+    }
+
+    public boolean isRecording(EntityPlayer player)
+    {
+        return this.sessions.get(player) != null;
+    }
+
+    public void startRecording(EntityPlayer player)
+    {
+        this.sessions.put(player, new SoundSession());
+    }
+
+    public SoundSession stopRecording(EntityPlayer player)
+    {
+        return this.sessions.remove(player);
     }
 
     @Override
@@ -46,21 +59,25 @@ public class SoundEventListener implements IWorldEventListener
     @Override
     public void playSoundToAllNearExcept(EntityPlayer player, SoundEvent soundIn, SoundCategory category, double x, double y, double z, float volume, float pitch)
     {
-        if (this.session != null)
+        if (!this.sessions.isEmpty())
         {
-            EntityPlayer thePlayer = this.mc.thePlayer;
-
-            double dx = thePlayer.posX - x;
-            double dy = thePlayer.posY - y;
-            double dz = thePlayer.posZ - z;
-            double distance = dx * dx + dy * dy + dz * dz;
-
-            distance = MathHelper.clamp_double(distance, 0, 30 * 30) / (30 * 30);
-            volume = volume * (float) (1 - distance);
-
-            if (volume > 0.00001F)
+            for (Map.Entry<EntityPlayer, SoundSession> entry : this.sessions.entrySet())
             {
-                this.session.record(this.frame, soundIn.getSoundName().toString(), volume, pitch);
+                EntityPlayer thePlayer = entry.getKey();
+                SoundSession session = entry.getValue();
+
+                double dx = thePlayer.posX - x;
+                double dy = thePlayer.posY - y;
+                double dz = thePlayer.posZ - z;
+                double distance = dx * dx + dy * dy + dz * dz;
+
+                distance = MathHelper.clamp_double(distance, 0, 30 * 30) / (30 * 30);
+                volume = volume * (float) (1 - distance);
+
+                if (volume > 0.00001F)
+                {
+                    session.record(soundIn.getSoundName().toString(), volume, pitch);
+                }
             }
         }
     }
@@ -99,9 +116,18 @@ public class SoundEventListener implements IWorldEventListener
     @SubscribeEvent
     public void onPlayerTick(PlayerTickEvent event)
     {
+<<<<<<< Updated upstream
         if (event.side == Side.CLIENT && event.player == this.mc.thePlayer && event.phase == Phase.START)
+=======
+        if (event.side == Side.SERVER && event.phase == Phase.END)
+>>>>>>> Stashed changes
         {
-            this.frame++;
+            SoundSession session = this.sessions.get(event.player);
+
+            if (session != null)
+            {
+                session.frame++;
+            }
         }
     }
 }
