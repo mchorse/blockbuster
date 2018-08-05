6546d8806eca9750a64dc88223254d1d3cd8d4a9
McHorse <mchorselessrider@gmail.com>
2018-08-05 15:47:28 +0100

    Invert combined with looking property will make the head rotate across Z axis

--- src/main/java/mchorse/blockbuster/client/model/ModelCustom.java
+++ src/main/java/mchorse/blockbuster/client/model/ModelCustom.java
@@ -164,7 +164,15 @@ public class ModelCustom extends ModelBiped
             if (limb.limb.looking)
             {
                 limb.rotateAngleX += headPitch * 0.017453292F;
-                limb.rotateAngleY += netHeadYaw * 0.017453292F;
+
+                if (invert)
+                {
+                    limb.rotateAngleZ += netHeadYaw * 0.017453292F;
+                }
+                else
+                {
+                    limb.rotateAngleY += netHeadYaw * 0.017453292F;
+                }
             }
 
             if (limb.limb.swinging)
