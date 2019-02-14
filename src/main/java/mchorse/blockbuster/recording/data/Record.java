85b057558c91d8350944882f024943d139d608b8
McHorse <mchorselessrider@gmail.com>
2019-02-14 11:17:44 +0000

    Fix weird crash related to index out of bounds frames (reported by STH)

--- src/main/java/mchorse/blockbuster/recording/data/Record.java
+++ src/main/java/mchorse/blockbuster/recording/data/Record.java
@@ -106,7 +106,7 @@ public class Record
     }
 
     /**
-     * Get an action by given tick and index 
+     * Get an action on given tick and index 
      */
     public Action getAction(int tick, int index)
     {
@@ -121,6 +121,19 @@ public class Record
         }
 
         return null;
+    }
+
+    /**
+     * Get frame on given tick 
+     */
+    public Frame getFrame(int tick)
+    {
+        if (tick >= this.frames.size() || tick < 0)
+        {
+            return null;
+        }
+
+        return this.frames.get(tick);
     }
 
     /**
