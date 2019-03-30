3ef2677cf34309101652178ed3280977b2658f3e
McHorse <mchorselessrider@gmail.com>
2019-03-30 09:08:02 +0000

    Adapt to new Metamorph

--- src/main/java/mchorse/blockbuster_pack/morphs/CustomMorph.java
+++ src/main/java/mchorse/blockbuster_pack/morphs/CustomMorph.java
@@ -305,7 +305,7 @@ public class CustomMorph extends AbstractMorph
 
         if (target.worldObj.isRemote)
         {
-            this.parts.updateBodyLimbs(target);
+            this.parts.updateBodyLimbs(target, cap);
         }
 
         super.update(target, cap);
