8fabcaaa04f396af5925cd7cabc8353b9f9c573c
McHorse <mchorselessrider@gmail.com>
2019-05-02 10:03:42 +0100

    Fix the issue with hardcoded friction

--- src/main/java/mchorse/blockbuster/common/entity/EntityGunProjectile.java
+++ src/main/java/mchorse/blockbuster/common/entity/EntityGunProjectile.java
@@ -1,16 +1,24 @@
 package mchorse.blockbuster.common.entity;
 
+import java.util.List;
+
 import io.netty.buffer.ByteBuf;
 import mchorse.blockbuster.Blockbuster;
 import mchorse.blockbuster.common.GunInfo;
 import mchorse.metamorph.api.MorphManager;
 import mchorse.metamorph.api.morphs.AbstractMorph;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumFacing.Axis;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
@@ -51,8 +59,128 @@ public class EntityGunProjectile extends EntityThrowable implements IEntityAddit
     @Override
     public void onUpdate()
     {
-        super.onUpdate();
+        this.lastTickPosX = this.posX;
+        this.lastTickPosY = this.posY;
+        this.lastTickPosZ = this.posZ;
 
+        if (!this.worldObj.isRemote)
+        {
+            this.setFlag(6, this.isGlowing());
+        }
+
+        this.onEntityUpdate();
+
+        if (this.throwableShake > 0)
+        {
+            --this.throwableShake;
+        }
+
+        Vec3d position = new Vec3d(this.posX, this.posY, this.posZ);
+        Vec3d next = new Vec3d(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
+        RayTraceResult result = this.worldObj.rayTraceBlocks(position, next);
+
+        if (result != null)
+        {
+            next = new Vec3d(result.hitVec.xCoord, result.hitVec.yCoord, result.hitVec.zCoord);
+        }
+
+        Entity entity = null;
+        List<Entity> list = this.worldObj.getEntitiesWithinAABBExcludingEntity(this, this.getEntityBoundingBox().addCoord(this.motionX, this.motionY, this.motionZ).expandXyz(1.0D));
+        double d0 = 0.0D;
+
+        for (int i = 0; i < list.size(); ++i)
+        {
+            Entity current = list.get(i);
+
+            if (current.canBeCollidedWith())
+            {
+                AxisAlignedBB box = current.getEntityBoundingBox().expandXyz(0.30000001192092896D);
+                RayTraceResult ray = box.calculateIntercept(position, next);
+
+                if (ray != null)
+                {
+                    double d1 = position.squareDistanceTo(ray.hitVec);
+
+                    if (d1 < d0 || d0 == 0.0D)
+                    {
+                        entity = current;
+                        d0 = d1;
+                    }
+                }
+            }
+        }
+
+        if (entity != null)
+        {
+            result = new RayTraceResult(entity);
+        }
+
+        if (result != null)
+        {
+            if (result.typeOfHit == RayTraceResult.Type.BLOCK && this.worldObj.getBlockState(result.getBlockPos()).getBlock() == Blocks.PORTAL)
+            {
+                this.setPortal(result.getBlockPos());
+            }
+            else
+            {
+                if (!net.minecraftforge.common.ForgeHooks.onThrowableImpact(this, result)) this.onImpact(result);
+            }
+        }
+
+        this.posX += this.motionX;
+        this.posY += this.motionY;
+        this.posZ += this.motionZ;
+        float distance = MathHelper.sqrt_double(this.motionX * this.motionX + this.motionZ * this.motionZ);
+        this.rotationYaw = (float) (MathHelper.atan2(this.motionX, this.motionZ) * (180D / Math.PI));
+
+        for (this.rotationPitch = (float) (MathHelper.atan2(this.motionY, distance) * (180D / Math.PI)); this.rotationPitch - this.prevRotationPitch < -180.0F; this.prevRotationPitch -= 360.0F)
+        {}
+
+        while (this.rotationPitch - this.prevRotationPitch >= 180.0F)
+        {
+            this.prevRotationPitch += 360.0F;
+        }
+
+        while (this.rotationYaw - this.prevRotationYaw < -180.0F)
+        {
+            this.prevRotationYaw -= 360.0F;
+        }
+
+        while (this.rotationYaw - this.prevRotationYaw >= 180.0F)
+        {
+            this.prevRotationYaw += 360.0F;
+        }
+
+        this.rotationPitch = this.prevRotationPitch + (this.rotationPitch - this.prevRotationPitch) * 0.2F;
+        this.rotationYaw = this.prevRotationYaw + (this.rotationYaw - this.prevRotationYaw) * 0.2F;
+        float friction = this.props == null ? 1 : this.props.friction;
+        float gravity = this.getGravityVelocity();
+
+        if (this.isInWater())
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                this.worldObj.spawnParticle(EnumParticleTypes.WATER_BUBBLE, this.posX - this.motionX * 0.25D, this.posY - this.motionY * 0.25D, this.posZ - this.motionZ * 0.25D, this.motionX, this.motionY, this.motionZ, new int[0]);
+            }
+
+            friction *= 0.8F;
+        }
+
+        this.motionX *= friction;
+        this.motionY *= friction;
+        this.motionZ *= friction;
+
+        if (!this.hasNoGravity())
+        {
+            this.motionY -= gravity;
+        }
+
+        this.setPosition(this.posX, this.posY, this.posZ);
+        this.updateProjectile();
+    }
+
+    private void updateProjectile()
+    {
         if (this.worldObj.isRemote && this.updatePos > 0)
         {
             double d0 = this.posX + (this.targetX - this.posX) / this.updatePos;
@@ -184,8 +312,6 @@ public class EntityGunProjectile extends EntityThrowable implements IEntityAddit
     @SideOnly(Side.CLIENT)
     public void setPositionAndRotationDirect(double x, double y, double z, float yaw, float pitch, int posRotationIncrements, boolean teleport)
     {
-        this.setRotation(yaw, pitch);
-
         double dx = this.posX - x;
         double dy = this.posY - y;
         double dz = this.posZ - z;
