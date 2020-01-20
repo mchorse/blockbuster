2058013e6da73c0311b6fd620e0db678ec87d348
McHorse <mchorselessrider@gmail.com>
2020-01-20 12:54:30 +0000

    Add image morph cropping options

--- src/main/java/mchorse/blockbuster_pack/client/gui/GuiImageMorph.java
+++ src/main/java/mchorse/blockbuster_pack/client/gui/GuiImageMorph.java
@@ -1,7 +1,9 @@
 package mchorse.blockbuster_pack.client.gui;
 
 import mchorse.blockbuster.client.gui.dashboard.GuiDashboard;
+import mchorse.blockbuster.client.textures.GifTexture;
 import mchorse.blockbuster_pack.morphs.ImageMorph;
+import mchorse.mclib.client.gui.framework.GuiTooltip;
 import mchorse.mclib.client.gui.framework.elements.GuiButtonElement;
 import mchorse.mclib.client.gui.framework.elements.GuiTexturePicker;
 import mchorse.mclib.client.gui.framework.elements.GuiTrackpadElement;
@@ -40,6 +42,11 @@ public class GuiImageMorph extends GuiAbstractMorph<ImageMorph>
         public GuiButtonElement<GuiCheckBox> lighting;
         public GuiButtonElement<GuiCheckBox> billboard;
 
+        public GuiTrackpadElement left;
+        public GuiTrackpadElement right;
+        public GuiTrackpadElement top;
+        public GuiTrackpadElement bottom;
+
         public GuiImageMorphPanel(Minecraft mc, GuiImageMorph editor)
         {
             super(mc, editor);
@@ -77,6 +84,15 @@ public class GuiImageMorph extends GuiAbstractMorph<ImageMorph>
             });
             this.picker.setVisible(false);
 
+            this.left = new GuiTrackpadElement(mc, I18n.format("blockbuster.gui.image.left"), (value) -> this.morph.cropping.x = value.intValue());
+            this.left.setLimit(Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+            this.right = new GuiTrackpadElement(mc, I18n.format("blockbuster.gui.image.right"), (value) -> this.morph.cropping.w = value.intValue());
+            this.right.setLimit(Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+            this.top = new GuiTrackpadElement(mc, I18n.format("blockbuster.gui.image.top"), (value) -> this.morph.cropping.y = value.intValue());
+            this.top.setLimit(Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+            this.bottom = new GuiTrackpadElement(mc, I18n.format("blockbuster.gui.image.bottom"), (value) -> this.morph.cropping.h = value.intValue());
+            this.bottom.setLimit(Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+
             this.texture.resizer().parent(this.area).set(10, 10, 115, 20);
             this.scale.resizer().relative(this.texture.resizer()).set(0, 25, 115, 20);
             this.shaded.resizer().relative(this.scale.resizer()).set(0, 25, 80, 11);
@@ -84,7 +100,12 @@ public class GuiImageMorph extends GuiAbstractMorph<ImageMorph>
             this.billboard.resizer().relative(this.lighting.resizer()).set(0, 16, 80, 11);
             this.picker.resizer().parent(this.area).set(10, 10, 0, 0).w(1, -20).h(1, -20);
 
-            this.children.add(this.texture, this.scale, this.shaded, this.lighting, this.billboard, this.picker);
+            this.left.resizer().relative(this.billboard.resizer()).set(0, 11 + 30, 115, 20);
+            this.right.resizer().relative(this.left.resizer()).set(0, 25, 115, 20);
+            this.top.resizer().relative(this.right.resizer()).set(0, 25, 115, 20);
+            this.bottom.resizer().relative(this.top.resizer()).set(0, 25, 115, 20);
+
+            this.children.add(this.texture, this.scale, this.shaded, this.lighting, this.billboard, this.left, this.right, this.top, this.bottom, this.picker);
         }
 
         @Override
@@ -96,6 +117,25 @@ public class GuiImageMorph extends GuiAbstractMorph<ImageMorph>
             this.shaded.button.setIsChecked(morph.shaded);
             this.lighting.button.setIsChecked(morph.lighting);
             this.billboard.button.setIsChecked(morph.billboard);
+
+            this.left.setValue(morph.cropping.x);
+            this.right.setValue(morph.cropping.w);
+            this.top.setValue(morph.cropping.y);
+            this.bottom.setValue(morph.cropping.h);
+        }
+
+        @Override
+        public void draw(GuiTooltip tooltip, int mouseX, int mouseY, float partialTicks)
+        {
+            this.font.drawStringWithShadow(I18n.format("blockbuster.gui.image.crop"), this.left.area.x, this.left.area.y - 12, 0xffffff);
+
+            GifTexture.bindTexture(this.morph.texture);
+            int w = this.morph.getWidth();
+            int h = this.morph.getHeight();
+
+            this.font.drawStringWithShadow(I18n.format("blockbuster.gui.image.dimensions", w, h), this.bottom.area.x, this.bottom.area.y + 25, 0xaaaaaa);
+
+            super.draw(tooltip, mouseX, mouseY, partialTicks);
         }
     }
 }
