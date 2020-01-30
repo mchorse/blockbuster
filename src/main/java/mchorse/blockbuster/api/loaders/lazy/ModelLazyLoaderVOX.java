63066908d7b6d9d84e980285bac5928bd379b3ef
McHorse <mchorselessrider@gmail.com>
2020-01-30 11:14:47 +0000

    Scale the geometry rather than the pose

--- src/main/java/mchorse/blockbuster/api/loaders/lazy/ModelLazyLoaderVOX.java
+++ src/main/java/mchorse/blockbuster/api/loaders/lazy/ModelLazyLoaderVOX.java
@@ -82,8 +82,6 @@ public class ModelLazyLoaderVOX extends ModelLazyLoaderJSON
 
 		data.providesObj = true;
 		data.providesMtl = true;
-		data.scale[0] = data.scale[1] = data.scale[2] = data.scaleGui = 1 / 16F;
-		data.scale[0] *= -1;
 
 		blocky.setSize(1, 1, 1);
 		data.poses.put("flying", blocky.clone());
