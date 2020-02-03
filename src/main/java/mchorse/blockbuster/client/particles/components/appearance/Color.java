98aa42857bb9dce21372eea7526fdec10b76bf8f
McHorse <mchorselessrider@gmail.com>
2020-02-03 22:32:26 +0000

    Day 6: Implement color gradients

--- src/main/java/mchorse/blockbuster/client/particles/components/appearance/Color.java
+++ src/main/java/mchorse/blockbuster/client/particles/components/appearance/Color.java
@@ -3,6 +3,7 @@ package mchorse.blockbuster.client.particles.components.appearance;
 import mchorse.blockbuster.client.particles.emitter.BedrockParticle;
 import mchorse.blockbuster.client.particles.molang.MolangParser;
 import mchorse.blockbuster.client.particles.molang.expressions.MolangExpression;
+import mchorse.mclib.utils.Interpolations;
 
 import java.util.List;
 
@@ -33,6 +34,14 @@ public abstract class Color
 			particle.b = (float) this.b.get();
 			particle.a = (float) this.a.get();
 		}
+
+		public void lerp(BedrockParticle particle, float factor)
+		{
+			particle.r = Interpolations.lerp(particle.r, (float) this.r.get(), factor);
+			particle.g = Interpolations.lerp(particle.r, (float) this.g.get(), factor);
+			particle.b = Interpolations.lerp(particle.r, (float) this.b.get(), factor);
+			particle.a = Interpolations.lerp(particle.r, (float) this.a.get(), factor);
+		}
 	}
 
 	public static class Gradient extends Color
@@ -49,7 +58,41 @@ public abstract class Color
 		@Override
 		public void compute(BedrockParticle particle)
 		{
-			/* TODO: implement */
+			int length = this.stops.size();
+
+			if (length == 0)
+			{
+				particle.r = particle.g = particle.b = particle.a = 1;
+
+				return;
+			}
+			else if (length == 1)
+			{
+				this.stops.get(0).color.compute(particle);
+
+				return;
+			}
+
+			double factor = this.interpolant.get();
+
+			for (int i = 0; i < length; i ++)
+			{
+				ColorStop stop = this.stops.get(i);
+
+				if (stop.stop < factor)
+				{
+					stop.color.compute(particle);
+
+					if (i < length - 1)
+					{
+						ColorStop next = this.stops.get(i + 1);
+
+						next.color.lerp(particle, (float) (factor - stop.stop) / (next.stop - stop.stop));
+
+						return;
+					}
+				}
+			}
 		}
 
 		public static class ColorStop
