1e380b54f6b2ab6fa44d03ca3a49073392792d48
McHorse <mchorselessrider@gmail.com>
2020-04-09 18:06:37 +0100

    Add Blockbuster categories

--- src/main/java/mchorse/blockbuster_pack/BlockbusterSection.java
+++ src/main/java/mchorse/blockbuster_pack/BlockbusterSection.java
@@ -6,6 +6,7 @@ import mchorse.blockbuster.api.Model;
 import mchorse.blockbuster.api.ModelPack;
 import mchorse.blockbuster.network.Dispatcher;
 import mchorse.blockbuster.network.common.structure.PacketStructureListRequest;
+import mchorse.blockbuster_pack.morphs.CustomMorph;
 import mchorse.blockbuster_pack.morphs.ImageMorph;
 import mchorse.blockbuster_pack.morphs.ParticleMorph;
 import mchorse.blockbuster_pack.morphs.RecordMorph;
@@ -13,9 +14,11 @@ import mchorse.blockbuster_pack.morphs.SequencerMorph;
 import mchorse.mclib.utils.resources.RLUtils;
 import mchorse.metamorph.api.creative.categories.MorphCategory;
 import mchorse.metamorph.api.creative.sections.MorphSection;
+import mchorse.metamorph.api.morphs.AbstractMorph;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
+import org.apache.commons.io.FilenameUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,28 +33,73 @@ public class BlockbusterSection extends MorphSection
 	{
 		super(title);
 
-		this.extra = new MorphCategory(this, "extra");
-		this.structures = new MorphCategory(this, "structures");
+		this.extra = new MorphCategory(this, "Blockbuster extra");
+		this.structures = new MorphCategory(this, "Blockbuster structures");
 
 		/* Adding some default morphs which don't need to get reloaded */
 		ImageMorph image = new ImageMorph();
 
 		image.texture = RLUtils.create("blockbuster", "textures/gui/icon.png");
 
-		this.extra.add(new ImageMorph());
+		this.extra.add(image);
 		this.extra.add(new ParticleMorph());
 		this.extra.add(new SequencerMorph());
 		this.extra.add(new RecordMorph());
 	}
 
-	public void add(String id, Model model)
+	public void add(String key, Model model)
 	{
-		System.out.println("Add: " + id + " " + model.name);
+		String path = this.getCategoryId(key);
+		CustomMorph morph = new CustomMorph();
+
+		morph.name = "blockbuster." + key;
+		morph.model = model;
+
+		MorphCategory category = this.models.get(path);
+
+		if (category == null)
+		{
+			category = new MorphCategory(this, path.isEmpty() ? "Blockbuster models" : "Blockbuster models (" + path + ")");
+			this.models.put(path, category);
+			this.categories.add(category);
+		}
+
+		category.add(morph);
 	}
 
-	public void remove(String id)
+	public void remove(String key)
 	{
-		System.out.println("Remove: " + id);
+		String path = this.getCategoryId(key);
+		String name = "blockbuster." + key;
+		MorphCategory category = this.models.get(path);
+		AbstractMorph morph = null;
+
+		for (AbstractMorph m : category.getMorphs())
+		{
+			if (m.name.equals(name))
+			{
+				morph = m;
+
+				break;
+			}
+		}
+
+		if (morph != null)
+		{
+			category.remove(morph);
+		}
+	}
+
+	private String getCategoryId(String key)
+	{
+		if (key.contains("/"))
+		{
+			key = FilenameUtils.getPath(key);
+
+			return key.substring(0, key.length() - 1);
+		}
+
+		return "";
 	}
 
 	@Override
