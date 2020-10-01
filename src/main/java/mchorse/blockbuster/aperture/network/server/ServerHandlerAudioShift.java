38adf4d914c1dc2120024d0e75fd6806b1c78117
McHorse <mchorselessrider@gmail.com>
2020-10-01 19:04:59 +0100

    Fix audio shift edited from camera editor not being saved (reported by Lynax)

--- src/main/java/mchorse/blockbuster/aperture/network/server/ServerHandlerAudioShift.java
+++ src/main/java/mchorse/blockbuster/aperture/network/server/ServerHandlerAudioShift.java
@@ -1,5 +1,6 @@
 package mchorse.blockbuster.aperture.network.server;
 
+import mchorse.blockbuster.CommonProxy;
 import mchorse.blockbuster.aperture.network.common.PacketAudioShift;
 import mchorse.blockbuster.audio.AudioState;
 import mchorse.blockbuster.recording.scene.Scene;
@@ -17,6 +18,13 @@ public class ServerHandlerAudioShift extends ServerMessageHandler<PacketAudioShi
 		{
 			scene.audioShift = message.shift;
 			scene.sendAudio(AudioState.SET, scene.getTick());
+
+			try
+			{
+				CommonProxy.scenes.save(scene.getId(), scene);
+			}
+			catch (Exception e)
+			{}
 		}
 	}
 }
