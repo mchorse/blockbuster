598a5d7d9e33bc4175e8e542f398d04f931acc19
McHorse <mchorselessrider@gmail.com>
2020-10-08 09:35:36 +0100

    Remove unnecessary code

--- src/main/java/mchorse/blockbuster/client/render/tileentity/TileEntityDirectorRenderer.java
+++ src/main/java/mchorse/blockbuster/client/render/tileentity/TileEntityDirectorRenderer.java
@@ -1,10 +1,7 @@
 package mchorse.blockbuster.client.render.tileentity;
 
-import mchorse.blockbuster.Blockbuster;
-import mchorse.blockbuster.common.block.BlockDirector;
 import mchorse.blockbuster.common.tileentity.TileEntityDirector;
 import mchorse.mclib.client.Draw;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
@@ -22,8 +19,6 @@ public class TileEntityDirectorRenderer extends TileEntitySpecialRenderer<TileEn
         /* Debug render (so people could find the block, lmao) */
         if (mc.gameSettings.showDebugInfo && !mc.gameSettings.hideGUI)
         {
-            IBlockState state = mc.world.getBlockState(te.getPos());
-            boolean playing = state.getBlock() == Blockbuster.directorBlock ? state.getValue(BlockDirector.PLAYING) : false;
             int shader = GL11.glGetInteger(GL20.GL_CURRENT_PROGRAM);
 
             if (shader != 0)
@@ -36,14 +31,7 @@ public class TileEntityDirectorRenderer extends TileEntitySpecialRenderer<TileEn
             GlStateManager.disableTexture2D();
             GlStateManager.enableBlend();
 
-            if (playing)
-            {
-                Draw.cube(x + 0.25F, y + 0.25F, z + 0.25F, x + 0.75F, y + 0.75F, z + 0.75F, 0, 1, 0, 0.5F);
-            }
-            else
-            {
-                Draw.cube(x + 0.25F, y + 0.25F, z + 0.25F, x + 0.75F, y + 0.75F, z + 0.75F, 1, 0, 0, 0.5F);
-            }
+            Draw.cube(x + 0.25F, y + 0.25F, z + 0.25F, x + 0.75F, y + 0.75F, z + 0.75F, 1, 0, 0, 0.5F);
 
             GlStateManager.disableBlend();
             GlStateManager.enableTexture2D();
