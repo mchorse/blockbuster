9a325f992719c1b147b08584e17c5f739cbf3736
McHorse <mchorselessrider@gmail.com>
2020-10-31 10:45:23 +0000

    Refactor texture editor using GuiCanvasEditor

--- src/main/java/mchorse/blockbuster/client/gui/dashboard/panels/model_editor/utils/GuiTextureCanvas.java
+++ src/main/java/mchorse/blockbuster/client/gui/dashboard/panels/model_editor/utils/GuiTextureCanvas.java
@@ -2,11 +2,9 @@ package mchorse.blockbuster.client.gui.dashboard.panels.model_editor.utils;
 
 import mchorse.blockbuster.api.ModelLimb;
 import mchorse.blockbuster.client.gui.dashboard.panels.model_editor.tabs.GuiModelLimbs;
-import mchorse.mclib.client.gui.framework.elements.GuiElement;
-import mchorse.mclib.client.gui.framework.elements.buttons.GuiButtonElement;
 import mchorse.mclib.client.gui.framework.elements.buttons.GuiIconElement;
 import mchorse.mclib.client.gui.framework.elements.input.GuiTrackpadElement;
-import mchorse.mclib.client.gui.framework.elements.utils.GuiCanvas;
+import mchorse.mclib.client.gui.framework.elements.utils.GuiCanvasEditor;
 import mchorse.mclib.client.gui.framework.elements.utils.GuiContext;
 import mchorse.mclib.client.gui.framework.elements.utils.GuiDraw;
 import mchorse.mclib.client.gui.utils.Area;
@@ -15,20 +13,15 @@ import mchorse.mclib.client.gui.utils.Icons;
 import mchorse.mclib.client.gui.utils.keys.IKey;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
 
-public class GuiTextureCanvas extends GuiCanvas
+public class GuiTextureCanvas extends GuiCanvasEditor
 {
-	public GuiElement editor;
 	public GuiTrackpadElement x;
 	public GuiTrackpadElement y;
 	public GuiIconElement close;
 
 	public GuiModelLimbs panel;
-	public int w;
-	public int h;
 
 	public GuiTextureCanvas(Minecraft mc, GuiModelLimbs panel)
 	{
@@ -38,10 +31,6 @@ public class GuiTextureCanvas extends GuiCanvas
 
 		this.close = new GuiIconElement(mc, Icons.CLOSE, (b) -> this.toggleVisible());
 		this.close.flex().relative(this).x(1F, -25).y(5);
-
-		/* TODO: move to GuiCanvas */
-		this.editor = new GuiElement(mc);
-		this.editor.flex().relative(this).xy(1F, 1F).w(130).anchor(1F, 1F).column(5).stretch().vertical().padding(10);
 
 		this.x = new GuiTrackpadElement(mc, (value) ->
 		{
@@ -63,55 +52,17 @@ public class GuiTextureCanvas extends GuiCanvas
 		this.markContainer();
 	}
 
-	/* TODO: move to GuiCanvas */
-	public void setSize(int w, int h)
+	@Override
+	protected boolean shouldDrawCanvas(GuiContext context)
 	{
-		this.w = w;
-		this.h = h;
-
-		this.scaleX.set(0, 2);
-		this.scaleY.set(0, 2);
-		this.scaleX.view(-this.w / 2, this.w / 2, this.area.w, 20);
-		this.scaleY.view(-this.h / 2, this.h / 2, this.area.h, 20);
-
-		double min = Math.min(this.scaleX.zoom, this.scaleY.zoom);
-
-		this.scaleX.zoom = min;
-		this.scaleY.zoom = min;
+		return this.panel.getPanel().modelRenderer.texture != null;
 	}
 
 	@Override
-	protected void drawCanvas(GuiContext context)
+	protected void drawCanvasFrame(GuiContext guiContext)
 	{
-		this.area.draw(0xff2f2f2f);
-
-		Area area = this.calculate(-this.w / 2, -this.h / 2, this.w / 2, this.h / 2);
-
-		Gui.drawRect(area.x - 1, area.y - 1, area.ex() + 1, area.ey() + 1, 0xff181818);
-		GlStateManager.color(1, 1, 1, 1);
-
 		ResourceLocation location = this.panel.getPanel().modelRenderer.texture;
-
-		if (location == null)
-		{
-			return;
-		}
-
-		int ox = (this.area.x - area.x) % 16;
-		int oy = (this.area.y - area.y) % 16;
-
-		Area processed = new Area();
-		processed.copy(this.area);
-		processed.offsetX(ox < 0 ? 16 + ox : ox);
-		processed.offsetY(oy < 0 ? 16 + oy : oy);
-		processed.clamp(area);
-		Icons.CHECKBOARD.renderArea(area.x, area.y, area.w, area.h);
-
-		GlStateManager.alphaFunc(GL11.GL_GREATER, 0);
-		GlStateManager.enableBlend();
-		GlStateManager.enableAlpha();
-
-		area = this.calculate(-this.w / 2, -this.h / 2, this.w / 2, this.h / 2);
+		Area area = this.calculate(-this.w / 2, -this.h / 2, this.w / 2, this.h / 2);
 
 		this.mc.renderEngine.bindTexture(location);
 		GuiDraw.drawBillboard(area.x, area.y, 0, 0, area.w, area.h, area.w, area.h);
@@ -141,8 +92,6 @@ public class GuiTextureCanvas extends GuiCanvas
 
 		Gui.drawRect(area.x, area.y, area.ex(), area.ey(), 0x55ff00ff);
 
-		area = this.calculateRelative(lx, ly, lx + ld * 2 + lw * 2, ly + ld + lh);
-
 		/* Left and right */
 		area = this.calculateRelative(lx, ly + ld, lx + ld, ly + ld + lh);
 
@@ -152,35 +101,18 @@ public class GuiTextureCanvas extends GuiCanvas
 
 		Gui.drawRect(area.x, area.y, area.ex(), area.ey(), 0x55ffff00);
 
+		/* Holes */
+		area = this.calculateRelative(lx, ly, lx + ld, ly + ld);
+
+		Gui.drawRect(area.x, area.y, area.ex(), area.ey(), 0xdd000000);
+
+		area = this.calculateRelative(lx + ld * 2 + lw, ly, lx + ld * 2 + lw * 2, ly + ld);
+
+		Gui.drawRect(area.x, area.y, area.ex(), area.ey(), 0xdd000000);
+
 		/* Outline */
 		area = this.calculateRelative(lx, ly, lx + ld * 2 + lw * 2, ly + ld + lh);
 
 		GuiDraw.drawOutline(area.x, area.y, area.ex(), area.ey(), 0xffff0000);
-
-		GlStateManager.color(1F, 1F, 1F);
-		GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
-	}
-
-	private Area calculateRelative(int a, int b, int c, int d)
-	{
-		return this.calculate(-this.w / 2 + a, -this.h / 2 + b, -this.w / 2 + c, -this.h / 2 + d);
-	}
-
-	/* TODO: move to GuiCanvas */
-	private Area calculate(int a, int b, int c, int d)
-	{
-		int x1 = (int) Math.round(this.scaleX.to(a) + this.area.mx());
-		int y1 = (int) Math.round(this.scaleY.to(b) + this.area.my());
-		int x2 = (int) Math.round(this.scaleX.to(c) + this.area.mx());
-		int y2 = (int) Math.round(this.scaleY.to(d) + this.area.my());
-
-		int x = x1;
-		int y = y1;
-		int fw = x2 - x;
-		int fh = y2 - y;
-
-		Area.SHARED.set(x, y, fw, fh);
-
-		return Area.SHARED;
 	}
 }
