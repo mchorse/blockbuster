0df921306b5bfede6c270b08c3b5a8c52bb48c5a
McHorse <mchorselessrider@gmail.com>
2021-01-02 20:15:52 +0000

    Minor tweaks to /model combine command

--- src/main/java/mchorse/blockbuster/commands/model/SubCommandModelCombine.java
+++ src/main/java/mchorse/blockbuster/commands/model/SubCommandModelCombine.java
@@ -14,6 +14,7 @@ import mchorse.mclib.utils.resources.TextureProcessor;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.server.MinecraftServer;
 
 import javax.imageio.ImageIO;
@@ -40,6 +41,11 @@ public class SubCommandModelCombine extends CommandBase
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
+        if (args.length < 1)
+        {
+            throw new WrongUsageException(this.getUsage(sender));
+        }
+
         List<MultiResourceLocation> toExport = new ArrayList<MultiResourceLocation>();
         List<FolderEntry> entries = new ArrayList<FolderEntry>();
 
