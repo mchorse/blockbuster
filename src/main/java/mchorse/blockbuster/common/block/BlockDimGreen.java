eb1f5dc6e0ca7777716653ffa63c005cea555ac6
NyaNLI <nyaplus@qq.com>
2021-11-26 18:06:21 +0800

    Fix BlockDimGreen

--- src/main/java/mchorse/blockbuster/common/block/BlockDimGreen.java
+++ src/main/java/mchorse/blockbuster/common/block/BlockDimGreen.java
@@ -1,10 +1,13 @@
 package mchorse.blockbuster.common.block;
 
+import net.minecraft.block.BlockSlab;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -32,5 +35,23 @@ public class BlockDimGreen extends BlockGreen
     public float getAmbientOcclusionLightValue(IBlockState state)
     {
         return 1;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public int getPackedLightmapCoords(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        int i = source.getCombinedLight(pos, state.getLightValue(source, pos));
+
+        if (i == 0 && state.getBlock() instanceof BlockSlab)
+        {
+            pos = pos.down();
+            state = source.getBlockState(pos);
+            return source.getCombinedLight(pos, state.getLightValue(source, pos));
+        }
+        else
+        {
+            return i;
+        }
     }
 }
