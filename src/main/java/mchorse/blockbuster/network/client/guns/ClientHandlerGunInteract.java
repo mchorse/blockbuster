8123993c1f7c8ed4ecf78b242265312db0d1e73f
Evanechecssss <evan.ko@mail.ru>
2022-01-08 23:59:15 +0500

    hot fix

--- src/main/java/mchorse/blockbuster/network/client/guns/ClientHandlerGunInteract.java
+++ src/main/java/mchorse/blockbuster/network/client/guns/ClientHandlerGunInteract.java
@@ -1,16 +1,22 @@
 package mchorse.blockbuster.network.client.guns;
 
+import mchorse.blockbuster.common.GunProps;
 import mchorse.blockbuster.common.item.ItemGun;
 import mchorse.blockbuster.network.common.guns.PacketGunInfo;
 import mchorse.blockbuster.network.common.guns.PacketGunInteract;
+import mchorse.blockbuster.utils.NBTUtils;
 import mchorse.mclib.network.ClientMessageHandler;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Date;
 
 /**
  * \* User: Evanechecssss
@@ -31,9 +37,13 @@ public class ClientHandlerGunInteract extends ClientMessageHandler<PacketGunInte
             }
             ItemGun gun = (ItemGun) packetGunInteract.itemStack.getItem();
             Entity entity = entityPlayerSP.world.getEntityByID(packetGunInteract.id);
+            GunProps props = NBTUtils.getGunProps(packetGunInteract.itemStack);
+    
             if (entity instanceof EntityPlayer)
             {
-                gun.shootIt(packetGunInteract.itemStack,entityPlayerSP, entity.world);
+                if (props.getGUNState()== ItemGun.GunState.READY_TO_SHOOT && props.timeBetweenShoot == 0) {
+                    gun.shootIt(packetGunInteract.itemStack, entityPlayerSP, entity.world);
+                }
             }
         }
     }
