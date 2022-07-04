d3386ec34f2e578c66e5918e5a8a14928eca7435
Chryfi <71967555+Chryfi@users.noreply.github.com>
2022-07-04 13:34:59 +0200

    Fix removeParentSpace not correctly being set when activating billboard

--- src/main/java/mchorse/blockbuster_pack/client/gui/GuiImageMorph.java
+++ src/main/java/mchorse/blockbuster_pack/client/gui/GuiImageMorph.java
@@ -145,6 +145,8 @@ public class GuiImageMorph extends GuiAbstractMorph<ImageMorph>
 
                 if (b.isToggled())
                 {
+                    this.morph.removeParentScaleRotation = true;
+
                     this.removeParentScaleRotation.toggled(true);
                 }
             });
