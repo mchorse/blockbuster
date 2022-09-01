b274ed4f83027588110dc97be658aa418521c775
Chryfi <71967555+Chryfi@users.noreply.github.com>
2022-09-01 14:50:15 +0200

    Fixed actor always render not working since renderLast changes

--- src/main/java/mchorse/blockbuster/core/transformers/RenderGlobalTransformer.java
+++ src/main/java/mchorse/blockbuster/core/transformers/RenderGlobalTransformer.java
@@ -85,28 +85,101 @@ public class RenderGlobalTransformer extends ClassTransformer
 
     private void processRenderEntities(MethodNode method)
     {
+        LabelNode releaseLabel = null;
+        LabelNode renderEntityLabel = null;
+        LabelNode lastLabel = null;
+        boolean captureNext = false;
+        int renderCounter = 0;
         Iterator<AbstractInsnNode> it = method.instructions.iterator();
+        boolean insertedRenderLastCall = false;
+        boolean foundAllForAddEntitiesHook = false;
 
         while (it.hasNext())
         {
             AbstractInsnNode node = it.next();
 
+            if (!foundAllForAddEntitiesHook && node instanceof LabelNode)
+            {
+                lastLabel = (LabelNode) node;
+
+                if (captureNext && renderEntityLabel == null)
+                {
+                    renderEntityLabel = lastLabel;
+                    captureNext = false;
+                }
+            }
+
             if (node instanceof MethodInsnNode)
             {
                 MethodInsnNode methodInsnNode = (MethodInsnNode) node;
 
-                /* this should indicate the end of the method (3rd method before end) */
+                if (!foundAllForAddEntitiesHook)
+                {
+                    if (CoreClassTransformer.checkName(methodInsnNode.owner, "bzf", "net/minecraft/client/renderer/entity/RenderManager")
+                            && CoreClassTransformer.checkName(methodInsnNode.name, "a", "renderEntityStatic")
+                            && CoreClassTransformer.checkName(methodInsnNode.desc, "(Lvg;FZ)V", "(Lnet/minecraft/entity/Entity;FZ)V"))
+                    {
+                        if (renderCounter == 1)
+                        {
+                            captureNext = true;
+                        }
+
+                        renderCounter += 1;
+                    }
+
+                    if (CoreClassTransformer.checkName(methodInsnNode.owner, "et$b", "net/minecraft/util/math/BlockPos$PooledMutableBlockPos")
+                            && CoreClassTransformer.checkName(methodInsnNode.name, "t", "release")
+                            && methodInsnNode.desc.equals("()V"))
+                    {
+                        releaseLabel = lastLabel;
+
+                        foundAllForAddEntitiesHook = true;
+                    }
+                }
+
+                /* this should indicate the end of the method renderEntities (postRenderDamagedBlocks is the 3rd method before end) */
                 if (CoreClassTransformer.checkName(methodInsnNode.owner, "buy", "net/minecraft/client/renderer/RenderGlobal")
                     && CoreClassTransformer.checkName(methodInsnNode.name, "v", "postRenderDamagedBlocks")
-                    && CoreClassTransformer.checkName(methodInsnNode.desc, "()V", "()V"))
+                    && methodInsnNode.desc.equals("()V"))
                 {
-                    /* Render last entities */
+                    /* Render last entities after postRenderDamagedBlocks to avoid OpenGL states that were meant for damagedBlocks */
                     method.instructions.insert(methodInsnNode, new MethodInsnNode(Opcodes.INVOKESTATIC, "mchorse/blockbuster/client/RenderingHandler", "renderLastEntities", "()V", false));
 
-                    System.out.println("BBCoreMod: successfully patched renderEntities!");
+                    insertedRenderLastCall = true;
 
-                    return;
+                    break;
                 }
+            }
+        }
+
+        if (renderEntityLabel != null && releaseLabel != null)
+        {
+            /* In non-Optifine Minecraft, the index of the entity variable is 27,
+             * however due to Optifine modifications, it's another index, but it
+             * should be the last local variable... */
+            final String entity = CoreClassTransformer.get("Lvg;", "Lnet/minecraft/entity/Entity;");
+            int localIndex = 0;
+
+            for (LocalVariableNode var : method.localVariables)
+            {
+                if (var.desc.equals(entity))
+                {
+                    localIndex = Math.max(localIndex, var.index);
+                }
+            }
+
+            /* Add render entity */
+            InsnList list = new InsnList();
+
+            list.add(new VarInsnNode(Opcodes.ALOAD, localIndex));
+            list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "mchorse/blockbuster/client/RenderingHandler", "addRenderedEntity", "(" + entity + ")V", false));
+
+            /* avoid memory leak - if renderLast method is not called, the list of rendered entities would not be cleared */
+            if (insertedRenderLastCall)
+            {
+                method.instructions.insert(renderEntityLabel, list);
+
+                System.out.println("BBCoreMod: successfully patched renderEntities!");
             }
         }
     }
