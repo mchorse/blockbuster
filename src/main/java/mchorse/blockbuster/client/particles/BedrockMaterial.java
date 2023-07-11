8be0e42fe2ff6f6d24cf9cfc20552e70fb77b4a4
l89669 <l89669@hotmail.com>
2023-07-11 06:12:54 -0500

    Add support for materials in bedrock particles

--- src/main/java/mchorse/blockbuster/client/particles/BedrockMaterial.java
+++ src/main/java/mchorse/blockbuster/client/particles/BedrockMaterial.java
@@ -1,8 +1,11 @@
 package mchorse.blockbuster.client.particles;
 
+import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.opengl.GL11;
+
 public enum  BedrockMaterial
 {
-    OPAQUE("particles_opaque"), ALPHA("particles_alpha"), BLEND("particles_blend");
+    OPAQUE("particles_opaque"), ALPHA("particles_alpha"), BLEND("particles_blend"), ADDITIVE("particles_additive");
 
     public final String id;
 
@@ -23,4 +26,52 @@ public enum  BedrockMaterial
     {
         this.id = id;
     }
+
+    public void beginGL()
+    {
+        switch (this)
+        {
+            case OPAQUE:
+                GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+                GlStateManager.alphaFunc(GL11.GL_GREATER, 0F);
+                GlStateManager.disableBlend();
+                GlStateManager.enableAlpha();
+                break;
+            case ALPHA:
+                GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+                GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+                GlStateManager.disableBlend();
+                GlStateManager.enableAlpha();
+                break;
+            case BLEND:
+                GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+                GlStateManager.alphaFunc(GL11.GL_GREATER, 0.0F);
+                GlStateManager.enableBlend();
+                GlStateManager.enableAlpha();
+                break;
+            case ADDITIVE:
+                GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
+                GlStateManager.alphaFunc(GL11.GL_GREATER, 0.0F);
+                GlStateManager.enableBlend();
+                GlStateManager.enableAlpha();
+                break;
+        }
+    }
+
+    public void endGL()
+    {
+        switch (this)
+        {
+            case OPAQUE:
+            case ALPHA:
+            case BLEND:
+            case ADDITIVE:
+                GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+                GlStateManager.disableBlend();
+                GlStateManager.enableAlpha();
+                GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+                break;
+        }
+    }
+
 }
